1e542ec9ecd75ff9bcf2a27b5c35334ca3db4bc1
McGuive7 <McGuive7@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-18 00:03:57 +0000

    v1.0.0
    git-svn-id: https://plugins.svn.wordpress.org/equal-height-columns/trunk@1114939 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/class-equal-height-columns-admin.php
@@ -0,0 +1,256 @@
+<?php
+/**
+ * The dashboard-specific functionality of the plugin.
+ *
+ * Defines the plugin name, version, and two examples hooks for how to
+ * enqueue the dashboard-specific stylesheet and JavaScript.
+ *
+ * @package    Equal_Height_Columns
+ * @subpackage Equal_Height_Columns/admin
+ * @author     MIGHTYminnow, Mickey Kay, Braad Martin
+ */
+class Equal_Height_Columns_Admin {
+
+	/**
+	 * The ID of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string    $plugin_name    The ID of this plugin.
+	 */
+	private $plugin_name;
+
+	/**
+	 * The display name of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string    $plugin_display_name    The public name of this plugin.
+	 */
+	private $plugin_display_name;
+
+	/**
+	 * The version of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string    $version    The current version of this plugin.
+	 */
+	private $version;
+
+	/**
+	 * Options for this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      array    $options    The options stored for this plugin.
+	 */
+	private $options;
+
+	/**
+	 * Slug for main options group.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string    $this->options_group_slug    Slug for the main options group.
+	 */
+	private $options_group_slug;
+
+	/**
+	 * Initialize the class and set its properties.
+	 *
+	 * @since    1.0.0
+	 * @var      string    $plugin_name            The name of this plugin.
+	 * @var      string    $plugin_display_name    The public name of this plugin.
+	 * @var      string    $version                The version of this plugin.
+	 */
+	public function __construct( $plugin_name, $plugin_display_name, $version ) {
+
+		$this->plugin_name = $plugin_name;
+		$this->plugin_display_name = $plugin_display_name;
+		$this->version = $version;
+		$this->options_group_slug = 'element-groups';
+
+	}
+
+	/**
+	 * Register the stylesheets for the Dashboard.
+	 *
+	 * @since    1.0.0
+	 */
+	public function enqueue_styles() {
+
+		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/equal-height-columns-admin.css', array(), $this->version, 'all' );
+
+	}
+
+	/**
+	 * Register the JavaScript for the dashboard.
+	 *
+	 * @since    1.0.0
+	 */
+	public function enqueue_scripts() {
+
+		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/equal-height-columns-admin.js', array( 'jquery' ), $this->version, false );
+
+	}
+
+	/**
+	 * Add options page to admin menu.
+	 *
+	 * @since    1.0.0
+	 */
+	public function add_options_page() {
+
+		// Get the options stored for this plugin.
+		$this->options = get_option( $this->plugin_name );
+
+		add_options_page(
+            $this->plugin_display_name, // Page title
+            $this->plugin_display_name, // Menu title
+            'manage_options', // Capability
+            $this->plugin_name, // Menu slug
+            array( $this, 'output_options_page' )
+        );
+
+	}
+
+	/**
+	 * Output the contents of the options page.
+	 *
+	 * @since    1.0.0
+	 */
+	public function output_options_page() {
+		?>
+		<div class="wrap">
+            <?php screen_icon(); ?>
+            <h2><?php echo $this->plugin_display_name; ?></h2>
+            <form method="post" action="options.php">
+            <?php
+                settings_fields( $this->plugin_name );
+                do_settings_sections( $this->plugin_name );
+                $this->output_add_button();
+                submit_button();
+            ?>
+            </form>
+        </div>
+        <?php
+	}
+
+	function output_add_button() {
+		printf(
+			'<button type="button" class="button add-group">%s</button>',
+			__( '+ Add More', 'equal-height-columns' )
+		);
+	}
+
+	/**
+	 * Register the plugin settings.
+	 *
+	 * @since    1.0.0
+	 */
+	function register_settings() {
+
+		register_setting(
+            $this->plugin_name, // Option group
+            $this->plugin_name, // Option name
+            array( $this, 'validate' ) // Validate
+        );
+
+        add_settings_section(
+            'main-settings', // ID
+            __( '<br />Instructions', 'equal-height-columns' ), // Title
+            array( $this, 'output_element_section_info' ), // Callback
+            $this->plugin_name // Page
+        );
+
+        // Set number of fields to output (number of saved groups, or 1 if none are saved).
+        $field_count = count( $this->options[ $this->options_group_slug ] ) ? count( $this->options[ $this->options_group_slug ] ) : 1;
+
+        // Output correct number of fields.
+        for ( $i = 1; $i <= $field_count; $i++ ) {
+
+	        add_settings_field(
+	            'elements-group-' . $i, // ID
+	            __( 'Column Group #', 'equal-height-columns' ) . '<span class="index-number">' . $i . '<span>', // Title
+	            array( $this, 'output_elements_fields' ), // Callback
+	            $this->plugin_name, // Page
+	            'main-settings', // Section
+	            array( // Args
+	                'id' => $this->options_group_slug,
+	                'index' => $i,
+	            )
+	        );
+
+	    }
+
+	}
+
+	function validate( $input ) {
+
+		// Validate input fields.
+		foreach( $input[ $this->options_group_slug ] as $key => $group ) {
+
+			// Validate selector.
+			$selector = $group['selector'];
+
+			$input[ $this->options_group_slug ][ $key ]['selector'] = wp_strip_all_tags( $selector, true );
+
+			// Validate breakpoint.
+			$breakpoint = $group['breakpoint'];
+			$input[ $this->options_group_slug ][ $key ]['breakpoint'] = intval( $breakpoint );
+
+		}
+
+		return $input;
+	}
+
+	function output_element_section_info() {
+		?>
+		<p><?php _e( '<b>Selector</b> - jQuery/CSS selectors of the elements you wish to target. Examples: <code>.col-1, .col-2</code> or <code>.container .col</code>', 'equal-height-columns' ); ?></p>
+		<p><?php _e( '<b>Breakpoint</b> - The screen width <i>above</i> which you want equal heights to take effect.</code>', 'equal-height-columns' ); ?></p>
+		<br />
+		<?php
+	}
+
+	function output_elements_fields( $args ) {
+
+		// Selector input
+		$field_id = 'selector-' . $args['index'];
+		printf(
+            '<div class="ehc-input-wrapper"><label for="%s">%s</label> <input type="text" id="%s" name="%s[%s][%s][%s]" data-index="%s" value="%s" placeholder="%s" /></div>',
+            $field_id,
+            __( 'Selector:', 'equal-height-columns' ),
+            $field_id,
+            $this->plugin_name,
+            $args['id'],
+            $args['index'],
+            'selector',
+            $args['index'],
+            isset( $this->options[ $args['id'] ][ $args['index'] ]['selector'] ) ? esc_attr( $this->options[ $args['id'] ][ $args['index'] ]['selector'] ) : '',
+            __( '.col-1, .col-2, .col-3', 'equal-height-columns' )
+        );
+
+		// Breakpoint input
+		$field_id = 'breakpoint-' . $args['index'];
+        printf(
+            '<div class="ehc-input-wrapper"><label for="%s">%s</label> <input type="number" id="%s" name="%s[%s][%s][%s]" data-index="%s" value="%s" /> px</div>',
+            $field_id,
+            __( 'Breakpoint:', 'equal-height-columns' ),
+            $field_id,
+            $this->plugin_name,
+            $args['id'],
+            $args['index'],
+            'breakpoint',
+            $args['index'],
+            isset( $this->options[ $args['id'] ][ $args['index'] ]['breakpoint'] ) ? esc_attr( $this->options[ $args['id'] ][ $args['index'] ]['breakpoint'] ) : '768'
+        );
+
+        // Remove button
+        printf(
+            '<button type="button" class="button remove-group">%s</button>',
+            __( 'Remove', 'equal-height-columns' )
+        );
+
+	}
+}
---
+++ readme.txt
@@ -0,0 +1,54 @@
+=== Equal Height Columns ===
+Contributors:      McGuive7, MIGHTYminnow, Braad
+Donate link:       http://wordpress.org/plugins/equal-height-columns
+Tags:              equal, height, column, div, element, jQuery, JavaScript
+Requires at least: 3.5
+Tested up to:      4.1
+Stable tag:        1.0.0
+License:           GPLv2 or later
+License URI:       http://www.gnu.org/licenses/gpl-2.0.html
+
+Apply equal heights to uneven columns and elements.
+
+== Description ==
+
+Equal Height Columns lets you easily equalize the height of various columns and elements.
+
+= Features =
+* Target unlimited elements and element groups
+* Specify simple CSS/jQuery selectors to target elements
+* Specify breakpoint to kick in only at certain screen sizes
+* Easy to use admin interface
+* Fully responsive (automatically updates on resize and orientationchange events)
+* Works on mobile devices
+* Works across all modern browsers (including IE8)
+* Comes with custom event listener to manually trigger
+* Super small - 8kB jQuery file size
+* Trigger custom 'equalheight' event to force resize
+
+= Instructions =
+
+1. Navigate to **Settings > Equal Height Columns** in the WordPress admin.
+2. Enter a *selector* and *breakpoint* for the first **column group**.
+3. Add/remove column groups by clicking the "+ Add More" and "Remove" buttons.
+
+
+== Installation ==
+
+= Manual Installation =
+
+1. Upload the entire `/equal-height-columns` directory to the `/wp-content/plugins/` directory.
+2. Activate Equal Height Columns through the 'Plugins' menu in WordPress.
+
+== Screenshots ==
+1. The easy-to-use admin interface.
+
+== Changelog ==
+
+= 1.0.0 =
+* First release
+
+== Upgrade Notice ==
+
+= 1.0.0 =
+First Release
